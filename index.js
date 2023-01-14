bb363cba0e75ff7bc2ed76b72aee2a0145919244
tomas juarez <mokajua@gmail.com>
2023-01-14 00:21:19 -0300

    resposive

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
-const btn = document.querySelector('#botondeprueba')
-const desplegable = document.querySelector('#menu')
+const btnmenu = document.querySelector('#btnmenu');
+const menu = document.querySelector('#menu');
+
+const subMenuBtn = document.querySelectorAll(".submenu-btn")
+
+btnmenu.addEventListener("click", function(){
+    menu.classList.toggle("mostrar");
+})
+
+for (let i= 0; i < subMenuBtn.length; i++) {
+
+    subMenuBtn[i].addEventListener("click",function(){
+       
+            const subMenu = subMenuBtn[i].nextElementSibling;
+            const height = subMenu.scrollHeight;
+
+            if (window.innerWidth < 1024) {
+            
+                if (subMenu.classList.contains("desplegar")) {
+                    console.log("entra en el if")
+                    subMenu.classList.remove("desplegar");
+                    subMenu.removeAttribute("style");
+                } else{
+                    console.log("entra en el else ")
+                    subMenu.classList.add("desplegar")
+                    subMenu.style.height = height + "px"
+                }
+    
+               
 
 
-let mostrar = true;
+            }
 
-function mostrarMensaje (){
-    if (mostrar===true) {
-        desplegable.style.display= 'inline-block'
-        mostrar = false;
-    }else{
-        desplegable.style.display= 'none'
-        mostrar=true;
-    }
+            
+        
+    });
 }
